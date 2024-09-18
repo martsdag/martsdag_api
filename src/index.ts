ca71166ab14b29831fdc23636872126fa7303eaf
martsdag <clarte.lisse.31@gmail.com>
2024-09-18 18:35:13 +0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
+import express from 'express';
 
 const app = express();
 const port = process.env.PORT;
 const domainName = process.env.DOMAIN_NAME;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req, res) => {
   res.send('Sudoku!!!');
 });
 
 app.listen(port, () => {
-  console.log(`http:${domainName}${port}`);
+  console.log(`http://${domainName}:${port}`);
 });
