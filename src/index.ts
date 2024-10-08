ba55d83d73501dec2a4d8ae327c2e0ad2459c763
martsdag <clarte.lisse.31@gmail.com>
2024-10-08 17:21:16 +0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import { router } from './router';
 const port = process.env.PORT;
 const domainName = process.env.DOMAIN_NAME;
 
-export const app = express().use(express.json()).use(router).use(cors());
+export const app = express().use(express.json()).use(cors()).use(router);
 
 app.listen(port, () => {
   console.log(`http://${domainName}:${port}`);
