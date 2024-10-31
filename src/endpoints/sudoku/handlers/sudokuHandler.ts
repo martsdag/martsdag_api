578fd9e0a25c648dacabe9ef0efbc0110182ce06
martsdag <clarte.lisse.31@gmail.com>
2024-10-31 23:09:47 +0400

    renaming

---
+++ src/endpoints/sudoku/handlers/sudokuHandler.ts
@@ -0,0 +1,29 @@
+import { pick } from '@/utils/pick';
+import { getSudoku } from 'sudoku-gen';
+
+const DIFFICULTIES = Object.freeze({ EASY: 'easy', MEDIUM: 'medium', HARD: 'hard', EXPERT: 'expert' });
+const FORMATS = Object.freeze({ STRING: 'string', MATRIX: 'matrix' });
+
+export class Sudoku {
+  private sudoku: {
+    puzzle: string;
+    solution: string;
+  };
+
+  constructor(difficulty: (typeof DIFFICULTIES)[keyof typeof DIFFICULTIES]) {
+    this.sudoku = pick(getSudoku(difficulty), ['puzzle', 'solution']);
+  }
+
+  getResult(format: (typeof FORMATS)[keyof typeof FORMATS]) {
+    if (format === FORMATS.MATRIX) {
+      const stringToMatrix = (string: string) => (string.match(/.{9}/g) ?? []).map((rowOrCol) => rowOrCol.split(''));
+
+      return {
+        puzzle: stringToMatrix(this.sudoku.puzzle),
+        solution: stringToMatrix(this.sudoku.solution),
+      };
+    }
+
+    return this.sudoku;
+  }
+}
